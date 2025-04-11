1c056b2fe30b03784728756a1b7f11edb771ce37
snakegirl260989@gmail.com <snakegirl260989@gmail.com>
2025-04-11 19:42:50 +0600

    first auto test on Python

--- test_auto1.py
+++ test_auto1.py
@@ -20,3 +20,43 @@ def test_success_log_in():
     time.sleep(3)
 
     driver.quit()
+
+def test_locked_out_user():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+
+    # find username field and input data
+    driver.find_element(By.ID, "user-name").send_keys("locked_out_user")
+
+    time.sleep(2)
+
+    # find password field and input data
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+
+    time.sleep(2)
+
+    # click on button
+    driver.find_element(By.ID, "login-button").click()
+
+    time.sleep(2)
+
+    assert driver.find_element(By.XPATH, '//*[@id="login_button_container"]/div/form/div[3]/h3').text == "Epic sadface: Sorry, this user has been locked out."
+
+    driver.quit()
+
+def test_empty_fields():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+
+    # find username field and input data
+    driver.find_element(By.ID, "user-name").send_keys(" ")
+
+    # find password field and input data
+    driver.find_element(By.ID, "password").send_keys(" ")
+
+    # click on button
+    driver.find_element(By.ID, "login-button").click()
+
+    assert driver.find_element(By.XPATH, '//*[@id="login_button_container"]/div/form/div[3]/h3')
+
+    driver.quit()
