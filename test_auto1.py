4455010e1a74861d79fc2c2644c6a1a2ca158f6e
snakegirl260989@gmail.com <snakegirl260989@gmail.com>
2025-04-12 22:46:20 +0600

    first auto test on Python

--- test_auto1.py
+++ test_auto1.py
@@ -1,10 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
 
-# def test_locked_out_user():
-#     driver = webdriver.Chrome()
 def test_success_log_in():
     driver = webdriver.Chrome()
     driver.get("https://www.saucedemo.com/")
@@ -12,12 +9,9 @@ def test_success_log_in():
     driver.find_element(By.ID,"user-name").send_keys("standard_user")
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
 
-    time.sleep(3)
-
     driver.find_element(By.XPATH,"//*[@id='login-button']").click()
-    driver.get(url="https://www.saucedemo.com/inventory.html")
 
-    time.sleep(3)
+    assert "inventory" in driver.current_url
 
     driver.quit()
 
@@ -25,22 +19,12 @@ def test_locked_out_user():
     driver = webdriver.Chrome()
     driver.get("https://www.saucedemo.com/")
 
-    # find username field and input data
     driver.find_element(By.ID, "user-name").send_keys("locked_out_user")
-
-    time.sleep(2)
-
-    # find password field and input data
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
 
-    time.sleep(2)
-
-    # click on button
     driver.find_element(By.ID, "login-button").click()
 
-    time.sleep(2)
-
-    assert driver.find_element(By.XPATH, '//*[@id="login_button_container"]/div/form/div[3]/h3').text == "Epic sadface: Sorry, this user has been locked out."
+    assert driver.find_element(By.XPATH, '//*[@ data-test="error"]').text == "Epic sadface: Sorry, this user has been locked out."
 
     driver.quit()
 
@@ -48,15 +32,11 @@ def test_empty_fields():
     driver = webdriver.Chrome()
     driver.get("https://www.saucedemo.com/")
 
-    # find username field and input data
-    driver.find_element(By.ID, "user-name").send_keys(" ")
+    driver.find_element(By.ID, "user-name")
+    driver.find_element(By.ID, "password")
 
-    # find password field and input data
-    driver.find_element(By.ID, "password").send_keys(" ")
-
-    # click on button
     driver.find_element(By.ID, "login-button").click()
 
-    assert driver.find_element(By.XPATH, '//*[@id="login_button_container"]/div/form/div[3]/h3')
+    assert driver.find_element(By.XPATH, '//*[@Data-test="error"]')
 
     driver.quit()
